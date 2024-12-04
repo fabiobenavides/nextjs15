8ddc8368fcf9a86a45f5dc8f6830b85e76fe1997
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-12-04 15:41:00 -0600

    NewPost initial

--- 02-starting-project/src/components/PostsList.jsx
+++ 02-starting-project/src/components/PostsList.jsx
@@ -1,11 +1,15 @@
+import NewPost from "./NewPost";
 import Post from "./post";
 import classes from  './PostsList.module.css';
 
 export default function PostList() {
   return (
-    <ul className={classes.posts}>
-        <Post name={"Fabio"} text={"Learning React"}/>
-        <Post name={"Mauricio"} text={"knows a little of React"}/>
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+          <Post name={"Fabio"} text={"Learning React"}/>
+          <Post name={"Mauricio"} text={"knows a little of React"}/>
+      </ul>
+    </>
   )
 }
