1f9b4419285fbe11b4c71aefa423ee735b05e209
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-12-06 08:27:27 -0600

    Render conditional

--- 02-starting-project/src/components/PostsList.jsx
+++ 02-starting-project/src/components/PostsList.jsx
@@ -8,6 +8,7 @@ export default function PostList() {
 
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('');
+  const [modalIsVisible, setModalIsVisible] = useState(true);
 
   function changeBodyHandler(event) {
       console.log(event.target.value);
@@ -17,16 +18,20 @@ export default function PostList() {
   function changeAuthorHandler(event) {
     console.log(event.target.value);
     setAuthor(event.target.value);
-}
+  }
+
+  function hideModalHandler(event) {
+    setModalIsVisible(false);
+  }
 
   return (
     <>
-      <Modal>
-        <NewPost 
-          changeBodyHandler={changeBodyHandler} 
-          changeAuthorHandler={changeAuthorHandler} 
-        />
-      </Modal>
+      { modalIsVisible && <Modal onClose={hideModalHandler}>
+          <NewPost 
+            changeBodyHandler={changeBodyHandler} 
+            changeAuthorHandler={changeAuthorHandler} 
+          />
+      </Modal> }
       <ul className={classes.posts}>
           <Post name={author} text={body}/>
           <Post name={"Mauricio"} text={"knows a little of React"}/>
