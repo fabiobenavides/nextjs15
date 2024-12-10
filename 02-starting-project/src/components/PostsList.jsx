ac9d56758f006946c3945562491354b7b6630343
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-12-10 08:42:53 -0600

    Save and Get posts from an API

--- 02-starting-project/src/components/PostsList.jsx
+++ 02-starting-project/src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from  './PostsList.module.css';
@@ -7,11 +7,39 @@ import Modal from "./Modal";
 export default function PostList({ modalIsVisible, onHideModal }) {
 
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:8080/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
     // Use this way (arrow funct) if our new state depends on the previous state
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
+
+  if (isFetching) { 
+    return (
+      <div style={{ textAlign: 'center', color: 'white'} }>
+        <p>Loading the data</p>
+      </div>
+    )   
+  };
 
   return (
     <>
@@ -29,7 +57,7 @@ export default function PostList({ modalIsVisible, onHideModal }) {
         </ul>
       }
       {posts.length === 0 && (
-          <div style={{ textAling: 'center', color: 'white'} }>
+          <div style={{ textAlign: 'center', color: 'white'} }>
             <h2>There are no post yet.</h2>
           </div>  
       )}
