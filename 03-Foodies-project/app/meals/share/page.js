2ae42762223ac2120fb2cf60cd09ba891041308e
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-12-30 08:30:55 -0600

    Creating an header

---
+++ 03-Foodies-project/app/meals/share/page.js
@@ -0,0 +1,5 @@
+export default function MealsShare() {
+  return (
+    <div>Meals Share</div>
+  )
+}
--- 03-Foodies-project/app/page.js
+++ 03-Foodies-project/app/page.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
+      <p><Link href="/meals">Link to meals page</Link></p>
+      <p><Link href="/meals/share">Link to share meals page</Link></p>
+      <p><Link href="/community">Link to community page</Link></p>
     </main>
   );
 }
