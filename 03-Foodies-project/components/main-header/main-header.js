d311ac1076b24afef54d4c13ceac49603449d760
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-01-02 08:43:35 -0600

    First Client Components

--- 03-Foodies-project/components/main-header/main-header.js
+++ 03-Foodies-project/components/main-header/main-header.js
@@ -1,25 +1,32 @@
+"use client"
+
 import Link from 'next/link'
 import logoImg from '@/assets/logo.png'
-import classess from './main-header.module.css'
+import classes from './main-header.module.css'
 import Image from 'next/image'
 import MainHeaderBackground from './main-header-background'
+import { usePathname } from 'next/navigation'
 
 export default function MainHeader() {
+
+    const currentPathName = usePathname();
+
   return (
     <>
         <MainHeaderBackground />
-        <header className={classess.header}>
-            <Link className={classess.logo} href="/">
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
                 <Image src={logoImg} alt="A plate with food on it" priority />
                 NextLevel Food
             </Link>
-            <nav className={classess.nav}>
+            <nav className={classes.nav}>
                 <ul>
                     <li>
-                        <Link href="/meals">Browse meals</Link>
+                        {/* startsWith for nested pages */} 
+                        <Link href="/meals" className={currentPathName.startsWith('/meals') ? classes.active : undefined}>Browse meals</Link>
                     </li>
                     <li>
-                        <Link href="/community">Foodies community</Link>
+                        <Link href="/community" className={currentPathName === '/community' ? classes.active : undefined}>Foodies community</Link>
                     </li>
                 </ul>
             </nav>
