9743b64cad14f6a9ba6275a82c424e357605e5c9
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-01-07 08:59:07 -0600

    Load and show meails details

--- 03-Foodies-project/app/meals/[id]/page.js
+++ 03-Foodies-project/app/meals/[id]/page.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import Image from 'next/image'
+import classes from './page.module.css'
+import { getMealDetails } from '@/lib/meals'
 
-export default function MealDetail() {
+export default function MealDetail({ params }) {
+
+  const mealDetails = getMealDetails( params.id);
+
+  mealDetails.instructions = mealDetails.instructions.replace(/\n/g, '<br />');
+
   return (
-    <div>Meal Detail page</div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={mealDetails.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{mealDetails.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${mealDetails.creator_email}`}>{mealDetails.creator}</a>
+          </p>
+          <p className={classes.summary}>{mealDetails.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: mealDetails.instructions,
+        }}></p>
+      </main>
+    </>
   )
 }
