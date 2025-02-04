969dacc7b0e9a43e7aea9863984e19979b372175
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-02-04 08:04:43 -0600

    Improving the form with dynamic button behavior

--- 03-Foodies-project/components/meals/meals-form-submit.js
+++ 03-Foodies-project/components/meals/meals-form-submit.js
@@ -1,13 +1,13 @@
 'use client';
 
-import { useFormState } from "react-dom";
+import { useFormStatus } from "react-dom";
 
 export default function MealsFormSubmit() {
 
-  const { pending } = useFormState();
+  const { pending } = useFormStatus();
 
   return (
-    <button>
+    <button disabled={pending}>
         { pending ? 'Submitting...' : 'Share Meal' }
     </button>
   )
