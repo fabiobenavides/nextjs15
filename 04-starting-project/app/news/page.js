f871e6c9c7504ceb8a317be78eb0b7f786392296
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-02-10 08:16:52 -0600

    UI improves and loading news from array

--- 04-starting-project/app/news/page.js
+++ 04-starting-project/app/news/page.js
@@ -1,11 +1,24 @@
 import Link from 'next/link'
 import React from 'react'
+import { DUMMY_NEWS } from '@/dummy-news'
 
 export default function NewsPage() {
   return (
     <>
         <div>Welcome to my News Page</div>
-        <Link href="/news/test"> Test news link</Link>
+        <ul className='news-list'>
+          {DUMMY_NEWS.map((item) =>
+            <li key={item} >
+              <Link href={`/news/${item.slug}`}>
+                <img
+                  src={`/images/news/${item.image}`} 
+                  alt={item.title}
+                />
+                <span>{item.title}</span>
+              </Link>
+            </li>
+          )}
+        </ul>
     </>
   )
 }
--- 04-starting-project/components/main-header/main-header.js
+++ 04-starting-project/components/main-header/main-header.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import NavLink from './nav-link'
+import Link from 'next/link';
 
 export default function MainHeader() {
   return (
-    <header >
-        <nav >
-            <ul>
-                <li>
-                    <NavLink href="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink href="/news">News</NavLink>
-                </li>
-            </ul>
-        </nav>
+    <header id="main-header">
+      <div id="logo">
+        <Link href="/">NextNews</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/news">News</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
