fa668ee6147975dbc3a5c154cd4b09ef159f8a44
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-02-19 08:22:16 -0600

    New client component for navigation

--- 04-starting-project/components/main-header/nav-link.js
+++ 04-starting-project/components/main-header/nav-link.js
@@ -1,13 +1,17 @@
-//"use client"
+"use client"
+// Client components should be as small as possible
 
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 
 export default function NavLink({ href, children }) {
+
+  const currentPath = usePathname();
 
   return (
     <>
         {/* startsWith for nested pages */} 
-        <Link href={href}> {children}
+        <Link href={href} className={currentPath.startsWith(href) ? 'active' : undefined}> {children}
         </Link>
     </>
   )
