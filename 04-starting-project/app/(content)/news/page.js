45cb23567ce3b06e4d45177d24a6a8c6728afb85
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-02-27 07:59:01 -0600

    Fetchind data - Option 1

--- 04-starting-project/app/(content)/news/page.js
+++ 04-starting-project/app/(content)/news/page.js
@@ -1,11 +1,53 @@
+'use client'
+
 import NewsList from '@/components/news-list'
-import { DUMMY_NEWS } from '@/dummy-news'
+import { useEffect, useState } from 'react'
 
 export default function NewsPage() {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+
+    async function fetchNews() {
+
+      setIsLoading(true);
+      const response = await fetch('http://localhost:8080/news'); 
+
+      if (!response.ok) {
+        setError('Failed to fecht data');
+        setIsLoading(false);
+        return;
+      }
+
+      const news = await response.json();
+      setIsLoading(false);
+      setNews(news);
+    }
+    
+    fetchNews();
+
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading....</p>
+  }
+
+  if (error) {
+    return <p>{error}</p>
+  }
+
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
         <div>Welcome to my News Page</div>
-        <NewsList news={DUMMY_NEWS} />
+        {newsContent}
     </>
   )
 }
