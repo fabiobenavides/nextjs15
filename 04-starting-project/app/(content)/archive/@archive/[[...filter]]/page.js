521a7f3535bb10902246405123b7c4479bef84e5
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-03-04 08:09:08 -0600

    Using suspense for individual parts

--- 04-starting-project/app/(content)/archive/@archive/[[...filter]]/page.js
+++ 04-starting-project/app/(content)/archive/@archive/[[...filter]]/page.js
@@ -1,61 +1,88 @@
 import NewsList from '@/components/news-list'
 import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from '@/lib/news';
 import Link from 'next/link';
-import React from 'react';
+import React, { Suspense } from 'react';
+
+async function FilterHeader({year, month}) {
+
+  const links = await getAvailableNewsYears();
+  var months = '';
+  if (year) {
+    months = getAvailableNewsMonths(year);
+  }
+
+  //console.log("links:", links);
+  //console.log("months:", months);
+
+  if (year && !links.includes(year) 
+    || month && !months.includes(month)) {
+      throw new Error('Invalid filter');
+  }
+
+  return (
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {links.map(item => 
+            <li key={item}>
+              <Link href={`/archive/${item}`}>{item}</Link>
+              { year && year == item && months &&
+                <ul>
+                  {months.map(month => 
+                    <li key={month}>
+                      <Link href={`/archive/${item}/${month}`}>{month}</Link>  
+                    </li>
+                  )}    
+                </ul>
+              }
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+async function FilteredNews({year, month}) {
+  let news;
+
+  if (year && !month) {
+    news = await getNewsForYear(year);    
+  }
+
+  if (month) {
+    news = await getNewsForYearAndMonth(year, month);
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  return newsContent;
+}
 
 export default async function ArchiveYear({params}) {
 
     //const filters = React.use(params);
-    const links = await getAvailableNewsYears();
     const filter = params.filter;
-    var news = '';
-    var months = '';
-
     //console.log(filter);
-
     const selectedYear = filter?.[0];
     const selectedMonth = filter?.[1];
 
-    if (selectedYear) {
-      news = await getNewsForYear(selectedYear);
-      months = getAvailableNewsMonths(selectedYear);
-    }
-
-    if (selectedMonth)
-    {
-      news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
-    }
-
-    if (selectedYear && !links.includes(selectedYear) 
-      || selectedMonth && !months.includes(selectedMonth)) {
-        throw new Error('Invalid filter');
-    }
-
     return (
       <>
-        <header id="archive-header">
-          <nav>
-            <ul>
-              {links.map(item => 
-                <li key={item}>
-                  <Link href={`/archive/${item}`}>{item}</Link>
-                  { selectedYear && selectedYear == item && months &&
-                    <ul>
-                      {months.map(month => 
-                        <li key={month}>
-                          <Link href={`/archive/${item}/${month}`}>{month}</Link>  
-                        </li>
-                      )}    
-                    </ul>
-                  }
-                </li>
-              )}
-            </ul>
-          </nav>
-        </header>
-        { selectedYear && 
-          <NewsList news={news} />
+        { // A granual way to say that a part of the component is still loading 
         }
+        <Suspense fallback={<p>Loading filter...</p>}>
+          <FilterHeader year={selectedYear} month={selectedMonth} />
+        </Suspense>
+        { // A granual way to say that a part of the component is still loading 
+        }
+        <Suspense fallback={<p>Loading news...</p>}>
+          <FilteredNews year={selectedYear} month={selectedMonth} />
+        </Suspense>
       </>
   )
 }
