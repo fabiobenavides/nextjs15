aace05f308d1f02325c3e7687f0ad080b05cedaf
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-03-13 08:04:06 -0600

    RevalidatePath to solve prod cache issues

--- 06-mutating-data-project/app/actions/toogleLikeStatus.js
+++ 06-mutating-data-project/app/actions/toogleLikeStatus.js
@@ -5,5 +5,8 @@ import { revalidatePath } from "next/cache";
 
 export default async function toogleLikeStatus(postId) {
   await updatePostLikeStatus(postId, 2);
-  revalidatePath('/feed');
+  //update cache, revalite the feed page
+  //revalidatePath('/feed');
+  //update cache, revalidate all pages
+  revalidatePath('/', 'layout');
 }
