754e99adb3043219b8791defbaf8b454fd785671
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-03-14 08:09:03 -0600

    Other way to control data cache

--- 07-starting-project/app/messages/page.js
+++ 07-starting-project/app/messages/page.js
@@ -1,12 +1,14 @@
+import { unstable_noStore } from 'next/cache';
 import Messages from '@/components/messages';
 
+//export const revalidate = 5; //revalidate for the entire page in case there are multiples request
+//export const dynamic = 'force-dynamic'; //'force-dynamic' same as fetch: cache: 'no-store'
+
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    //cache: 'no-store', //next 14 default 'force-cache' -- next 15 default 'no-store' less agressive
-    next: {
-      revalidate: 5 //seconds for next revalidate cache
-    }
-  });
+
+  unstable_noStore(); //same as no-store
+
+  const response = await fetch('http://localhost:8080/messages');
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
