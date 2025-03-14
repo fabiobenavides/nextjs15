ceb5015dd2b8311fc2294a59a8b10da18127965f
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-03-14 07:59:17 -0600

    fetch options to handle cache

--- 07-starting-project/app/messages/page.js
+++ 07-starting-project/app/messages/page.js
@@ -1,7 +1,12 @@
 import Messages from '@/components/messages';
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages');
+  const response = await fetch('http://localhost:8080/messages', {
+    //cache: 'no-store', //next 14 default 'force-cache' -- next 15 default 'no-store' less agressive
+    next: {
+      revalidate: 5 //seconds for next revalidate cache
+    }
+  });
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
