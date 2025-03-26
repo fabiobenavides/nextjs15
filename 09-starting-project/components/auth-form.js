2e4305f3d0f5d53cec207475e236b21b2770eb8f
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-03-26 15:29:49 -0600

    Form action added

--- 09-starting-project/components/auth-form.js
+++ 09-starting-project/components/auth-form.js
@@ -1,8 +1,13 @@
+'use client';
+import { signup } from '@/actions/auth';
 import Link from 'next/link';
+import { useFormState } from 'react-dom';
+
 
 export default function AuthForm() {
+  const [state, formAction] = useFormState(signup, {});
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
@@ -14,6 +19,12 @@ export default function AuthForm() {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </p>
+      {state.errors && 
+        (<ul id='form-errors'>
+          {Object.keys(state.errors).map((error) => (
+            <li key={error}>{state.errors[error]}</li>
+          ))}
+        </ul>)}
       <p>
         <button type="submit">
           Create Account
