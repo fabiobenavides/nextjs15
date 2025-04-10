a3bc10391ddaca7bc4050367cd6ae71411dd5277
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-04-10 08:03:46 -0600

    Link programatically

--- 10-starting-project/pages/clients/[id]/index.js
+++ 10-starting-project/pages/clients/[id]/index.js
@@ -5,8 +5,19 @@ export default function ClientProjects() {
     const router = useRouter();
     console.log("router:", router.query);
     
+    function loadProjectHandler() {
+        // load data...
+        // router.push('/clients/[id]/[clientprojectid]', '/clients/max/project-a')
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: { id: 'max', clientprojectid: 'project-a' }
+        })
+    }
 
   return (
-    <div>Client Projects Page</div>
+    <div>
+      <h1>Client Projects Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
   )
 }
