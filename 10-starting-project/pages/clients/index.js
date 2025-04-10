0425c99daba1966bb9b974b1000329ef3cc0230c
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-04-10 07:49:32 -0600

    Using LInk

--- 10-starting-project/pages/clients/index.js
+++ 10-starting-project/pages/clients/index.js
@@ -1,6 +1,22 @@
+import Link from "next/link";
 
 export default function ClientsPage() {
+
+  const clients = [
+    { id: "max", name: "Max" },
+    { id: "emma", name: "Emma" },
+  ];
+
   return (
-    <div>Clients Page - List some clients</div>
+    <div>
+      <h1>Clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
--- 10-starting-project/pages/index.js
+++ 10-starting-project/pages/index.js
@@ -1,5 +1,15 @@
+import Link from 'next/link'
+
 export default function index() {
   return (
-    <div>Home page</div>
+    <div>
+      <h1>Home page</h1>
+      <ul>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
+    </div>
+    
   )
 }
