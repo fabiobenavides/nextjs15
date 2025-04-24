062a157e70d031807d4457ae40304a8e34fa3114
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-04-24 08:02:09 -0600

    Getting the data from an array

--- 12-project-time/pages/events/index.js
+++ 12-project-time/pages/events/index.js
@@ -1,19 +1,20 @@
 import Link from 'next/link'
+import { getAllEvents } from '../../dummy-data'
 
 export default function index() {
+
+    const data = getAllEvents();
+
+
   return (
     <div>
       <h1>Events page</h1>
       <ul>
-        <li>
-          <Link href="/events/1">Event 1</Link>
-        </li>
-        <li>
-          <Link href="/events/2">Event 2</Link>
-        </li>
-        <li>
-          <Link href="/events/3">Event 3</Link>
-        </li>
+        {data.map((event) => (
+          <li key={event.id}>
+            <Link href={`/events/${event.id}`}>{event.title}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   )
