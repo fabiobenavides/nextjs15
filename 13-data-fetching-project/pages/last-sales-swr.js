b215b8e64f17fdc93ddde3d9ec04a646e8cde20e
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-05-20 07:39:02 -0600

    Combine pre-fetch with client side fetch

--- 13-data-fetching-project/pages/last-sales-swr.js
+++ 13-data-fetching-project/pages/last-sales-swr.js
@@ -1,9 +1,9 @@
 import useSWR from 'swr';
 import { useState, useEffect } from 'react';
 
-export default function LastSalesPage() {
+export default function LastSalesPage(props) {
 
-    const [sales, setSales] = useState([]);
+    const [sales, setSales] = useState(props.sales);
     const { data, error, isLoading } = useSWR('https://nextjs-course-f2ad5-default-rtdb.firebaseio.com/sales.json', (url) => fetch(url).then(res => res.json()));
 
     useEffect(() => {
@@ -24,7 +24,7 @@ export default function LastSalesPage() {
     if (error) {
         return <p>Error: {error.message}</p>;
     }
-    if (isLoading || !sales || !data) {
+    if (isLoading && !sales && !data) {
         return <p>Loading...</p>;
     }
 
@@ -40,5 +40,28 @@ export default function LastSalesPage() {
             </ul>
         </div>
     )
+}
+
+export async function getStaticProps() {
+
+    return fetch('https://nextjs-course-f2ad5-default-rtdb.firebaseio.com/sales.json')
+        .then(response => response.json())
+        .then(data => {
+            const transformedSales = [];
+            for (const key in data) {
+                transformedSales.push({
+                    id: key,
+                    username: data[key].username,
+                    volume: data[key].volunme
+                });
+            }
+
+            return {
+                props: {
+                    sales: transformedSales
+                },
+                revalidate: 10 // 10 seconds
+            };
+        });
 }
 
