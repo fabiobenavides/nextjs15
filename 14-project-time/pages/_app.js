940529b39251ecc42c4a7b7868ae6fb26f64c08d
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-06-03 07:35:01 -0600

    Global metadata

--- 14-project-time/pages/_app.js
+++ 14-project-time/pages/_app.js
@@ -1,9 +1,14 @@
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Fabio Benavides NextJS Events</title>
+        <meta name="description" content="NextJS Events practice project" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
