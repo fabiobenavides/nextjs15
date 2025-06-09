6972a0161384a3c03e1adfbc13ca3ee0a8cc6006
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-06-09 07:52:01 -0600

    First API example

--- 16-api-project/pages/api/feedback.js
+++ 16-api-project/pages/api/feedback.js
@@ -1,13 +1,26 @@
+import path from 'path';
+import fs from 'fs';
+
 function handler(req, res) {
 
-    res.status(200).json({message: 'Hello World'});
-
-    
   if (req.method === 'POST') {
     const email = req.body.email;
     const feedback = req.body.feedback;
 
     console.log(email, feedback);
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      feedback
+    }
+
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+
+    res.status(201).json({message: 'Success!', feedback: newFeedback});
   }
 }
 
