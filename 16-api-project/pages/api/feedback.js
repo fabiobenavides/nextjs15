0b1c480c0e0b7d753ad8d409fba494132686fa07
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-06-09 07:56:42 -0600

    Get API

--- 16-api-project/pages/api/feedback.js
+++ 16-api-project/pages/api/feedback.js
@@ -4,7 +4,33 @@ import fs from 'fs';
 function handler(req, res) {
 
   if (req.method === 'POST') {
-    const email = req.body.email;
+    processFeedback(req, res);
+  } else if (req.method === 'GET') {
+    getFeedback(req, res);
+  } else {
+    res.status(404).json({message: 'Method not allowed'});
+  }
+
+}
+
+function buildFeedbackPath() {
+  return path.join(process.cwd(), 'data', 'feedback.json');
+}
+
+function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
+
+function getFeedback(req, res) {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+  res.status(200).json({feedback: data});
+}
+
+function processFeedback(req, res) {
+  const email = req.body.email;
     const feedback = req.body.feedback;
 
     console.log(email, feedback);
@@ -14,14 +40,12 @@ function handler(req, res) {
       feedback
     }
 
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
     data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
 
     res.status(201).json({message: 'Success!', feedback: newFeedback});
-  }
 }
 
 export default handler;
