50ad98a335b0557c40d5d9acc5115092da6a777d
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-07-22 08:00:43 -0600

    Markdown example

--- 19-Complete-App/components/posts/post-detail/post-content.js
+++ 19-Complete-App/components/posts/post-detail/post-content.js
@@ -1,15 +1,32 @@
 import Postheader from "./post-header";
 import classes from './post-content.module.css';
 import ReactMarkdown from 'react-markdown';
+import Image from "next/image";
 
 export default function PostContent({postData}) {
 
   const imagePath = `/images/posts/${postData.image}`;
 
+  const customRenderes = {
+    p(paragraph) {
+      const {node} = paragraph;
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image src={`/images/posts/${image.properties.src}`} alt={image.properties.alt} 
+              width={600} height={300}/>
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    }
+  };
+
   return (
     <article className={classes.content}>
         <Postheader title={postData.title} image={imagePath} />
-        <ReactMarkdown>{postData.content}</ReactMarkdown>
+        <ReactMarkdown components={customRenderes}>{postData.content}</ReactMarkdown>
     </article>
   )
 }
