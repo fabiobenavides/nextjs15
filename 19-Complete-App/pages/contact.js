4c2cf9b52aadf04828ce4d79790f499d566a821c
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-07-23 07:46:26 -0600

    Showing code using markdown

--- 19-Complete-App/pages/contact.js
+++ 19-Complete-App/pages/contact.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import ContactForm from "../components/contact/contact-form";
 
 export default function ContactPage() {
   return (
-    <div>ContactPage</div>
+    <ContactForm />
   )
 }
