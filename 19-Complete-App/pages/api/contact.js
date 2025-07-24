0c046f95cebe55e6e37bf1031ef35f92ac0d24f9
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2025-07-24 15:55:45 -0600

    Save msg in db init

--- 19-Complete-App/pages/api/contact.js
+++ 19-Complete-App/pages/api/contact.js
@@ -1,14 +1,15 @@
+import { MongoClient } from 'mongodb';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     if (req.method === 'POST') {
-        sendContactData(req, res);
+        await sendContactData(req, res);
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
 
-function sendContactData(req, res) {
+async function sendContactData(req, res) {
     const { email, name, message } = req.body;
 
     if (!email || !name || !message
@@ -20,5 +21,30 @@ function sendContactData(req, res) {
     // Here you would typically handle the form submission, e.g., save to a database or send an email
     console.log('Contact Form Submission:', { email, name, message });
 
+    let client;
+
+    try {
+        client = await MongoClient.connect(process.env.MONGODB_URI);
+        client.connect();
+    } catch (error) {
+        console.error('Database connection failed:', error);
+        return res.status(500).json({ message: 'Database connection failed' });
+    }
+    
+    const db = client.db;
+
+    try {
+            const result = await db.collection('messages').insertOne({
+                email,
+                name,
+                message,
+                createdAt: new Date()
+        });
+
+    } catch (error) {
+        console.error('Failed to insert message:', error);
+        return res.status(500).json({ message: 'Failed to insert message' });
+    }
+    
     res.status(201).json({ message: 'Message sent successfully!' });
 }
