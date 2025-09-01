34b7c72e1db86005457d0541eaa4d8e0761cb4b9
Fabio Benavides <fabio.benavides@gmail.com>
2025-09-01 08:04:44 -0600

    Store the user in mongodb

--- 21-Auth-App/lib/db.js
+++ 21-Auth-App/lib/db.js
@@ -1,4 +1,4 @@
-import { MongoClint} from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 export async function connectToDatabase() {
     const client = await MongoClient.connect(process.env.MONGODB_URI);
--- 21-Auth-App/pages/api/auth/signup.js
+++ 21-Auth-App/pages/api/auth/signup.js
@@ -1,7 +1,7 @@
 import { connectToDatabase } from '../../../lib/db';
+import { hashPassword } from '../../../lib/auth';
 
 export default async function handler(req, res) {
-  
 
     if (req.method !== 'POST') {
         return;
@@ -9,6 +9,28 @@ export default async function handler(req, res) {
 
     const { email, password } = req.body;
 
+    if (!email || !email.includes('@') || !password 
+        || password.trim().length < 7) {
+        res.status(422).json({ 
+            message: 'Invalid input - password should also be at least 7 characters long.' 
+        });
+        return;
+    }
+
     const client = await connectToDatabase();
     const db = client.db();
+    const existingUser = await db.collection('users').findOne({ email: email });
+    if (existingUser) {
+        res.status(422).json({ message: 'User exists already!' });
+        client.close();
+        return;
+    }
+
+    var hashedPassword = await hashPassword(password);
+    const result = await db.collection('users')
+        .insertOne({ email: email, password: hashedPassword });
+    
+    res.status(201).json({ message: 'Created user!' });
+    client.close();
+
 }
