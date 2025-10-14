511cc55219e45148cb7634403de2256d72775920
Fabio Benavides <fabio.benavides@gmail.com>
2025-10-14 07:29:39 -0600

    updates

--- 23-Summary/pages/[meetupId]/index.js
+++ 23-Summary/pages/[meetupId]/index.js
@@ -1,13 +1,42 @@
 
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-export default function MeetupDetails() {
+export default function MeetupDetails(props) {
   return (
     <MeetupDetail 
-        image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg"
-        title="First Meetup"
-        address="Some address 5, 12345 Some City"
-        description="This is a first meetup"
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
     />
   )
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+  //fetch data from an API
+  //...
+ return { 
+  props: {
+    meetupData: {
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",  
+      id: meetupId,
+      title: "A First Meetup",
+      address: "Some address 5, 12345 Some City",
+      description: "This is a first meetup",
+    }
+  }}
+} 
+
+export async function getStaticPaths() {
+  //fetch all meetups
+  //...
+  return {
+    fallback: false,
+    paths: [
+      { params: { meetupId: "m1" } },
+      { params: { meetupId: "m2" } },
+    ]
+  }
 }
